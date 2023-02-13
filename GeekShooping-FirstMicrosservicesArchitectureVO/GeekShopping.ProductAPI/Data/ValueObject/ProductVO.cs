77ef2194bddb551672fb4256a92c226269bbfb07
LEONARDO LISBOA <leonardo.lisboa@grupofasitec.com.br>
2023-02-13 18:03:45 -0300

    microsservicos e swagger

---
+++ GeekShooping-FirstMicrosservicesArchitectureVO/GeekShopping.ProductAPI/Data/ValueObject/ProductVO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.ProductAPI.Data.ValueObject
+{
+    public class ProductVO //é ume espelho da nossa entidade, da tabela mesmo. Crio o VO por segurança (pois sem ele o usuário consegue visualizar toda a estrutura do banco) e tbm pra facilitar a alteração de algo ai da tabela
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
