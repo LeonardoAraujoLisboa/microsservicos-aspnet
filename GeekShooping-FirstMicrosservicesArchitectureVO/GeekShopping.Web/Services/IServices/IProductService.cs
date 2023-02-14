d326ed0d144298566b458ac4f19eae2f29d77f54
LEONARDO LISBOA <leonardo.lisboa@grupofasitec.com.br>
2023-02-14 17:29:33 -0300

    Implementando métodos no front

---
+++ GeekShooping-FirstMicrosservicesArchitectureVO/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProducts();
+        Task<ProductModel> GetProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProduct(long id);
+    }
+}
